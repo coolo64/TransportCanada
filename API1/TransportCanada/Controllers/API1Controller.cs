a902d8275c142bf8c9e2af717805654edc3fa926
Luc <Lucbreault@hotmail.com>
2020-07-06 19:10:48 -0400

    Ajout du champs MANUFACTURER_RECALL_NO_TXT au JSON

--- API1/TransportCanada/Controllers/API1Controller.cs
+++ API1/TransportCanada/Controllers/API1Controller.cs
@@ -22,24 +22,24 @@ namespace TransportCanada.Controllers
         public async Task<IActionResult> Index([FromBody]List<Recall> recalls)
         {
             HttpClient HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "API1 App");
+            HttpClient.DefaultRequestHeaders.Add("accept", "application/json");
 
-            //foreach(Recall recall in recalls)
-            //{
-            Recall recall = recalls[0];
+            foreach(Recall recall in recalls)
+            {
+            //Recall recall = recalls[0];
                 if (!string.IsNullOrWhiteSpace(recall.recallNumber)) {
                     using (var response = await HttpClient.GetAsync("https://data.tc.gc.ca/v1.3/api/eng/vehicle-recall-database/sommaire-rappel/numero-de-rappel/" + recall.recallNumber + "?format=json"))
                     {
-                        if (!response.IsSuccessStatusCode)
+                        if (response.IsSuccessStatusCode)
                         {
-                            return BadRequest("data.tc.gc.ca returned " + response.StatusCode.ToString());
+                            string recalldata = await response.Content.ReadAsStringAsync();
+                            recall.MANUFACTURER_RECALL_NO_TXT = JsonConvert.DeserializeObject<Result>(recalldata).ResultSet.First().Where(x => x.Name.Equals("MANUFACTURER_RECALL_NO_TXT")).FirstOrDefault()?.Value.Literal;
                         }
-
-                        string recalldata = await response.Content.ReadAsStringAsync();
-                        recall.MANUFACTURER_RECALL_NO_TXT = JsonConvert.DeserializeObject<List<RecallSummary>>(recalldata).Where(x => x.Name.Equals("MANUFACTURER_RECALL_NO_TXT")).FirstOrDefault()?.Value.Literal;
 
                     }
                 }
-            //}
+            }
             
 
             return Json(recalls);
